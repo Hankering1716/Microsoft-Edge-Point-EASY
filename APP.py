be6d69ad9ce3e3cf5f7ba5b50210cbeab8aa4d79
Hankering1716 <116718014+Hankering1716@users.noreply.github.com>
2023-03-30 21:59:51 +0200

    Update APP.py

--- APP.py
+++ APP.py
@@ -20,4 +20,4 @@ print(len(words))
 for i in range(90):
   for i in range(1):
     word = random.choice(random_words(50,5))
-    browser.open(f"https://www.bing.com/search?q={word}&cvid=f930075a9e794d9fbedccf25fe597481&aqs=edge.0.69i59l4j0l2j69i60l3.1169j0j1&pglt=41&FORM=ANNTA1&PC=U531")
+    browser.open(f"https://www.bing.com/search?q={word}")
